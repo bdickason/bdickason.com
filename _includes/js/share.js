a20f26fbe3a20e2bfedc66e627b5952d8bde8844
Brad Dickason <dickason@gmail.com>
2021-01-20 20:51:11 -0800

    Added button to share

---
+++ _includes/js/share.js
@@ -0,0 +1,12 @@
+/* Show/hide sharing pop up */
+window.addEventListener('load', function(event) {
+
+    const sharing = window.location.hash;
+    console.log(sharing);
+
+    // Check if sharing = true
+
+    // Click 'share' (or url) = show sharing hash
+    // Click close = remove sharing hash
+  })
+  
