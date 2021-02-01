f2868a3053695188f5320a5fa64971a2a16b3495
Brad Dickason <dickason@gmail.com>
2021-01-31 22:49:28 -0800

    Model now opens and closes as expected

--- _includes/js/share.js
+++ _includes/js/share.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function(event) {
     // Modal to display sharing options 
     const shareWindow = document.getElementsByClassName('share-background')[0];
 
+    /* Events that open share window */
     // Listen for sharing button click *
     const shareButton = document.getElementById('share');
     shareButton.onclick = function() {
@@ -11,17 +12,32 @@ window.addEventListener('load', function(event) {
 
     // Check if user was sent from email w/ share permalink 
     if(window.location.hash == '#share') {
-        console.log('got here');
+        // Clear the hash so the page doesn't share on refresh
+        window.location.hash = '';  
+
         // Display sharing popup
         toggleVisibility(shareWindow);
     }
 
-    // Click 'share' (or url) = show sharing hash
-    // Click close = remove sharing hash
+    /* Events that close share window */
+    /* Click the 'x' button */
+    const closeButton = document.getElementById('close');
+    closeButton.onclick = function() {
+        toggleVisibility(shareWindow);
+    }
+
+    /* Click outside the modal */
+    shareWindow.onclick = function(e) {
+        if(e.target.className == 'share-background') {
+            toggleVisibility(shareWindow);
+        }
+    }
+
+    // Click close = close window
 });
 
 function toggleVisibility(modal) {
-    if(modal.style.display = 'none') {
+    if(modal.style.display == 'none' || modal.style.display == '') {
         modal.style.display = 'block';
     } else {
         modal.style.display = 'none';
