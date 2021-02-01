2fbdcba2fc2ff8fd18949ec7ac7b1a0362ff9f47
Brad Dickason <dickason@gmail.com>
2021-01-31 22:27:13 -0800

    Share button toggles cursor

--- _includes/js/share.js
+++ _includes/js/share.js
@@ -1,17 +1,29 @@
 /* Show/hide sharing pop up */
 window.addEventListener('load', function(event) {
-    // Check if url includes share 'permalink'
-    if(window.location.hash == '#share') {
-        console.log('sharing')
-        // Display sharing popup
-        
+    // Modal to display sharing options 
+    const shareWindow = document.getElementsByClassName('share-background')[0];
+
+    // Listen for sharing button click *
+    const shareButton = document.getElementById('share');
+    shareButton.onclick = function() {
+        toggleVisibility(shareWindow);
     }
 
-
-
-
-    // Check if sharing = true
+    // Check if user was sent from email w/ share permalink 
+    if(window.location.hash == '#share') {
+        console.log('got here');
+        // Display sharing popup
+        toggleVisibility(shareWindow);
+    }
 
     // Click 'share' (or url) = show sharing hash
     // Click close = remove sharing hash
 });
+
+function toggleVisibility(modal) {
+    if(modal.style.display = 'none') {
+        modal.style.display = 'block';
+    } else {
+        modal.style.display = 'none';
+    }
+}
